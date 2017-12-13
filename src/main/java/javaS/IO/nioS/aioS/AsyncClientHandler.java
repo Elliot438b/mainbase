28740dadbe18fc0074d01c080519f06b0d141aa1
evsward <iliuwb@163.com>
2017-12-13 15:12:30 +0800

    remove jdk1.6 api

---
+++ src/main/java/javaS/IO/nioS/aioS/AsyncClientHandler.java
@@ -0,0 +1,13 @@
+package javaS.IO.nioS.aioS;
+
+import javaS.IO.socketS.Base;
+
+public class AsyncClientHandler extends Base implements Runnable {
+
+  @Override
+  public void run() {
+    // TODO Auto-generated method stub
+
+  }
+
+}
---
+++ src/main/java/javaS/IO/nioS/aioS/AsyncServerHandler.java
@@ -0,0 +1,22 @@
+package javaS.IO.nioS.aioS;
+
+import javaS.IO.socketS.Base;
+
+/**
+ * 异步非阻塞服务器
+ * 
+ * @author Evsward
+ *
+ */
+public class AsyncServerHandler extends Base implements Runnable {
+
+  public AsyncServerHandler(){
+    
+  }
+  
+  @Override
+  public void run() {
+
+  }
+
+}
