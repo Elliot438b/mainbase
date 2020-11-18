7366a97e5a69a1facb9585ccb8b802b72c72cc10
liuwenbin24 <liuwenbin24@jd.com>
2020-11-18 16:18:07 +0800

    Resolve warning, package upgrade, and gitignore.

--- src/main/java/algorithms/bag/Bag.java
+++ src/main/java/algorithms/bag/Bag.java
@@ -5,73 +5,74 @@ import java.util.Iterator;
 // 定义一个背包集合，支持泛型，支持迭代
 public class Bag<Item> implements Iterable<Item> {
 
-    private class BagNode<Item> {
-        Item item;
-        BagNode next;
-    }
+	private class BagNode<T> {
+		Item item;
+		BagNode<Item> next;
+	}
 
-    BagNode head;
-    int size;
+	BagNode<Item> head;
+	int size;
 
-    @Override
-    public Iterator<Item> iterator() {
-        return new Iterator<Item>() {
-            BagNode node = head;
+	@Override
+	public Iterator<Item> iterator() {
+		return new Iterator<Item>() {
+			BagNode<Item> node = head;
 
-            @Override
-            public boolean hasNext() {
-                return node.next != null;
-            }
+			@Override
+			public boolean hasNext() {
+				return node.next != null;
+			}
 
-            @Override
-            public Item next() {
-                Item item = (Item) node.item;
-                node = node.next;
-                return item;
-            }
-        };
-    }
+			@Override
+			public Item next() {
+				Item item = (Item) node.item;
+				node = node.next;
+				return item;
+			}
+		};
+	}
 
-    public Bag() {
-        head = new BagNode();
-        size = 0;
-    }
+	public Bag() {
+		head = new BagNode<Item>();
+		size = 0;
+	}
 
-    // 往前插入
-    public void add(Item item) {
-        BagNode temp = new BagNode();
-        // 以下两行代码一定要声明，不可直接使用temp = head，那样temp赋值的是head的引用，对head的所有修改会直接同步到temp，temp就不具备缓存的功能，引发bug。。
-        temp.next = head.next;
-        temp.item = head.item;
-        head.item = item;
-        head.next = temp;
-        size++;
-    }
+	// 往前插入
+	public void add(Item item) {
+		BagNode<Item> temp = new BagNode<Item>();
+		// 以下两行代码一定要声明，不可直接使用temp =
+		// head，那样temp赋值的是head的引用，对head的所有修改会直接同步到temp，temp就不具备缓存的功能，引发bug。。
+		temp.next = head.next;
+		temp.item = head.item;
+		head.item = item;
+		head.next = temp;
+		size++;
+	}
 
-    public boolean isEmpty() {
-        return size == 0;
-    }
+	public boolean isEmpty() {
+		return size == 0;
+	}
 
-    public int size() {
-        return this.size;
-    }
+	public int size() {
+		return this.size;
+	}
 
-    public static void main(String[] args) {
-        Bag<String> bags = new Bag();
-        bags.add("hello");
-        bags.add("yeah");
-        bags.add("liu wen bin");
-        bags.add("seminar");
-        bags.add("1243");
-        System.out.println(bags.size);
+	public static void main(String[] args) {
+		Bag<String> bags = new Bag<>();
+		bags.add("hello");
+		bags.add("yeah");
+		bags.add("liu wen bin");
+		bags.add("seminar");
+		bags.add("1243");
+		System.out.println(bags.size);
 
 //        for (Iterator i = bags.iterator(); i.hasNext(); ) {
 //            System.out.println(i.next());
 //        }
 
-        // 由于Bag实现了Iterable接口，所以支持以下方式遍历
-        for (String a : bags) {
-            System.out.println(a);
-        }
-    }
+		// 由于Bag实现了Iterable接口，所以支持以下方式遍历
+		for (String a : bags) {
+			System.out.println(a);
+		}
+	}
 }
--- src/main/java/algorithms/graph/Digraph.java
+++ src/main/java/algorithms/graph/Digraph.java
@@ -4,108 +4,101 @@ import algorithms.bag.Bag;
 import ioutil.In;
 import ioutil.StdOut;
 
-import java.io.FileReader;
-
 public class Digraph {
-    private final int V;// 顶点总数，定义final，第一次初始化以后不可更改。
-    private int E;// 边总数
-    private Bag<Integer>[] adj;// {邻接表}顶点为数组下标，值为当前下标为顶点值所连通的顶点个数。
+	private final int V;// 顶点总数，定义final，第一次初始化以后不可更改。
+	private int E;// 边总数
+	private Bag<Integer>[] adj;// {邻接表}顶点为数组下标，值为当前下标为顶点值所连通的顶点个数。
 
-    public Digraph(int v) {
-        this.V = v;
-        this.E = 0;
-        adj = new Bag[V];
-        for (int i = 0; i < V; i++) {
-            adj[i] = new Bag<Integer>();
-        }
-    }
+	@SuppressWarnings("unchecked")
+	public Digraph(int v) {
+		this.V = v;
+		this.E = 0;
+		adj = new Bag[V];
+		for (int i = 0; i < V; i++) {
+			adj[i] = new Bag<Integer>();
+		}
+	}
 
-    public Digraph(In in) {
-        this(in.readInt());
-        int E = in.readInt();
-        for (int i = 0; i < E; i++) {
-            int v = in.readInt();
-            int w = in.readInt();
-            addEdge(v, w);
-        }
-    }
+	public Digraph(In in) {
+		this(in.readInt());
+		int E = in.readInt();
+		for (int i = 0; i < E; i++) {
+			int v = in.readInt();
+			int w = in.readInt();
+			addEdge(v, w);
+		}
+	}
 
-    public int V() {
-        return this.V;
-    }
+	public int V() {
+		return this.V;
+	}
 
-    public int E() {
-        return this.E;
-    }
+	public int E() {
+		return this.E;
+	}
 
-    /**
-     * v和w是两个顶点，中间加一条边，增加稠密度。
-     *
-     * @param v 大V是顶点总数，v是顶点值，所以并v不存在大小限制
-     * @param w 同上。
-     */
-    public void addEdge(int v, int w) {
-        adj[v].add(w);
-        E++;
-    }
+	/**
+	 * v和w是两个顶点，中间加一条边，增加稠密度。
+	 *
+	 * @param v 大V是顶点总数，v是顶点值，所以并v不存在大小限制
+	 * @param w 同上。
+	 */
+	public void addEdge(int v, int w) {
+		adj[v].add(w);
+		E++;
+	}
 
-    /**
-     * 返回一个顶点的连通顶点集合的迭代器
-     *
-     * @param v
-     * @return Bag本身就是迭代器，所以返回该顶点的连通顶点集合Bag即可。
-     */
-    public Iterable<Integer> adj(int v) {
-        return adj[v];
-    }
+	/**
+	 * 返回一个顶点的连通顶点集合的迭代器
+	 *
+	 * @param v
+	 * @return Bag本身就是迭代器，所以返回该顶点的连通顶点集合Bag即可。
+	 */
+	public Iterable<Integer> adj(int v) {
+		return adj[v];
+	}
 
-    /**
-     * 将图中所有方向反转
-     *
-     * @return 返回一个图将所有方向反转后的副本
-     */
-    public Digraph reverse() {
-        Digraph R = new Digraph(V);
-        for (int v = 0; v < V; v++) {
-            for (int w : adj[v]) {// 遍历原图中跟v顶点连通的顶点w。
-                R.addEdge(w, v);
-            }
-        }
-        return R;
-    }
+	/**
+	 * 将图中所有方向反转
+	 *
+	 * @return 返回一个图将所有方向反转后的副本
+	 */
+	public Digraph reverse() {
+		Digraph R = new Digraph(V);
+		for (int v = 0; v < V; v++) {
+			for (int w : adj[v]) {// 遍历原图中跟v顶点连通的顶点w。
+				R.addEdge(w, v);
+			}
+		}
+		return R;
+	}
 
-    /**
-     * 按照邻接表数组结构输出有向图内容
-     *
-     * @return
-     */
-    public String toString() {
-        String s = V + " vertices, " + E + " edges\n";
-        for (int v = 0; v < V; v++) {
-            s += v + ": ";
-            for (int w : this.adj(v)) {
-                s += w + " ";
-            }
-            s += "\n";
-        }
-        return s;
-    }
+	/**
+	 * 按照邻接表数组结构输出有向图内容
+	 *
+	 * @return
+	 */
+	public String toString() {
+		String s = V + " vertices, " + E + " edges\n";
+		for (int v = 0; v < V; v++) {
+			s += v + ": ";
+			for (int w : this.adj(v)) {
+				s += w + " ";
+			}
+			s += "\n";
+		}
+		return s;
+	}
 
-    public static void main(String[] args) {
-        Digraph d = new Digraph(5);
-        d.addEdge(0, 1);
-        d.addEdge(1, 0);
-        d.addEdge(2, 3);
-        d.addEdge(0, 4);
-        StdOut.println(d);
-        /**
-         输出：
-         5 vertices, 3 edges
-         0: 4 1
-         1: 0
-         2:
-         3:
-         4:
-         */
-    }
+	public static void main(String[] args) {
+		Digraph d = new Digraph(5);
+		d.addEdge(0, 1);
+		d.addEdge(1, 0);
+		d.addEdge(2, 3);
+		d.addEdge(0, 4);
+		StdOut.println(d);
+		/**
+		 * 输出： 5 vertices, 3 edges 0: 4 1 1: 0 2: 3: 4:
+		 */
+	}
 }
