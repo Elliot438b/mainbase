056404d8d599133bbb4ffb591e1ac525473dc8dc
liuwenbin <lw.wenbin@gmail.com>
2020-11-19 14:14:48 +0800

    test commit

---
+++ src/main/java/leecode/cpp/1.两数之和.cpp
@@ -0,0 +1,20 @@
+/*
+ * @lc app=leetcode.cn id=1 lang=cpp
+ *
+ * [1] 两数之和
+ */
+#include <vector>
+using namespace std;
+// @lc code=start
+class Solution
+{
+public:
+    vector<int> twoSum(vector<int> &nums, int target)
+    {
+        vector<int> result;
+        for (int i = 0; i < nums.size(); i++)
+        {
+        }
+    }
+};
+// @lc code=end
