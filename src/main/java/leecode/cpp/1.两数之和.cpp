bafc5bc6e5a6ec6c0f3a68ccddade1355bf2dc23
liuwenbin <lw.wenbin@gmail.com>
2020-11-19 15:40:13 +0800

    add twosum

--- src/main/java/leecode/cpp/1.两数之和.cpp
+++ src/main/java/leecode/cpp/1.两数之和.cpp
@@ -4,6 +4,10 @@
  * [1] 两数之和
  */
 #include <vector>
+#include <iostream>
+#include <string>
+#include <unordered_map>
+
 using namespace std;
 // @lc code=start
 class Solution
@@ -11,10 +15,45 @@ class Solution
 public:
     vector<int> twoSum(vector<int> &nums, int target)
     {
-        vector<int> result;
-        for (int i = 0; i < nums.size(); i++)
+        int n = nums.size();
+        for (int i = 0; i < n; ++i)
         {
+            for (int j = i + 1; j < n; ++j)
+            {
+                if (nums[i] + nums[j] == target)
+                {
+                    return {i, j};
+                }
+            }
         }
+        return {};
+    }
+
+    vector<int> twoSum2(vector<int> &nums, int target)
+    {
+        unordered_map<int, int> hashtable;
+        int n = nums.size();
+        for (int i = 0; i < n; ++i)
+        {
+            auto it = hashtable.find(target - nums[i]);
+            if (it != hashtable.end())
+            {
+                return {it->second, i};
+            }
+            hashtable[nums[i]] = i;
+        }
+        return {};
     }
 };
 // @lc code=end
+
+int main()
+{
+    vector<int> nums = {8, 3, 0, 4, 11, 2};
+    short int target = 5;
+    Solution S;
+    vector<int> result = S.twoSum2(nums, target);
+    cout << "hello, world!" << endl;
+    cout << result[0] << endl;
+    cout << result[1] << endl;
+}
